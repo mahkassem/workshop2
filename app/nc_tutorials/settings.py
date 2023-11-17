006dde91679cf4df50273cacd90576ab30d34da3
Mahmoud Kassem <mahmoudalikassem@outlook.com>
2023-11-17 23:05:35 +0200

    (fix) docker config & allowed hosts

--- app/nc_tutorials/settings.py
+++ app/nc_tutorials/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = "django-insecure-(j-#aqq@vs$3l#0(031z4_h(_*pa&ye#dt&x69_@(&a6(=+fpg
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    "ec2-18-117-216-96.us-east-2.compute.amazonaws.com",
+    "ec2-18-116-9-232.us-east-2.compute.amazonaws.com",
     "0.0.0.0",
     "localhost",
     "127.0.0.1",
